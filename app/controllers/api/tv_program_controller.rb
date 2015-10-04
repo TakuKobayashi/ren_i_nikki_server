6a563dac3a573477c03896eaee4217b8c98fcf4a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-10-04 14:26:40 +0900

    サーバー側おかしかったので修正

--- app/controllers/api/tv_program_controller.rb
+++ app/controllers/api/tv_program_controller.rb
@@ -3,8 +3,11 @@ class Api::TvProgramController < Api::BaseController
     expressions = [params[:expression0], params[:expression1], params[:expression2], params[:expression3], params[:expression4]].map(&:to_i)
     max = expressions.max
     cat = expressions.index(max)
-    face = @user.face_captures.create!(expression_category: FaceCapture.expression_categories.invert[cat])
-    target = FaceCapture.where.not(user_id: @user.id).where("created_at > ?", 30.second.ago).sample.try(:user)
+    face = FaceCapture.find_or_initialize_by(user_id: @user.id)
+    cats = FaceCapture.expression_categories.invert[cat]
+    face.expression_category = cats if cats.present?
+    face.save!
+    target = FaceCapture.where.not(user_id: @user.id).where("updated_at > ?", 30.second.ago).sample.try(:user)
     if target.present?
       Device.send_message({title: "奇跡が起こりました!", message: "今すぐ起こった奇跡を確認してみよう!!"}, [target.id, @user.id])
     end
